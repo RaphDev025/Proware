da06cf75902b48fdb6361d0145e76b7f213a34c6
RaphDev025 <raphdev23@gmail.com>
2023-12-03 22:20:57 +0800

    serve

--- frontend/src/Pages/Site/MyCart.jsx
+++ frontend/src/Pages/Site/MyCart.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
-
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import yellowLogo from 'assets/images/yellowed.png'
 const MyCart = () => {
     const [cart, setCart] = useState(null)
     useEffect(() => {
@@ -21,6 +23,29 @@ const MyCart = () => {
         fetchProducts();
     }, []);
 
+    // Function to handle decrease button
+    const handleDecrease = (itemId) => {
+        const selectedItem = cart.find((item) => item._id === itemId);
+        if (selectedItem.qty > 1) {
+            handleQuantityChange(itemId, selectedItem.qty - 1);
+        }
+    };
+
+    // Function to handle increase button
+    const handleIncrease = (itemId) => {
+        const selectedItem = cart.find((item) => item._id === itemId);
+        handleQuantityChange(itemId, selectedItem.qty + 1);
+    };
+
+    // Function to handle quantity change
+    const handleQuantityChange = (itemId, newQuantity) => {
+        setCart((prevCart) =>
+        prevCart.map((item) =>
+            item._id === itemId ? { ...item, qty: newQuantity } : item
+        )
+        );
+    };
+        
     return (
         <>
             <main className='mt-5 d-flex gap-2 px-5 py-3 h-100'>
@@ -30,9 +55,9 @@ const MyCart = () => {
                     {cart && cart.map((item) => (
                         <div key={item._id} className='border d-flex border-2 p-3'>
                             <img alt={''} height={'300px'} src={''} />
-                            <div className='d-flex w-100'>
-                                <div className='w-25'>
-                                    <img src={item.product_img} alt={item.item_name} />
+                            <div className='d-flex gap-3 w-100'>
+                                <div className='w-25 d-flex bg-dark-subtle opacity-50'>
+                                    <img src={item.product_img || yellowLogo} alt={item.item_name} />
                                 </div>
                                 <div className='d-flex flex-column w-75 justify-content-around'>
                                     <div>
@@ -42,9 +67,9 @@ const MyCart = () => {
                                     <div className='d-flex gap-3'>
                                         <div className='d-flex align-items-center'>
                                             QTY:
-                                            <button className='ms-2 m-0 px-1  rounded-2 button-qty'>-</button>
+                                            <button onClick={() => handleDecrease(item._id)} className='ms-2 m-0 px-1 rounded-2 button-qty'>-</button>
                                             <p className='qty-indicator m-0 px-3 border-top border-1 border-black border-bottom'>{item.qty}</p>
-                                            <button className='m-0 px-1  rounded-2 button-qty'>+</button>
+                                            <button onClick={() => handleIncrease(item._id)} className='m-0 px-1 rounded-2 button-qty'>+</button>
                                         </div>
                                         <div>
                                             
@@ -52,7 +77,7 @@ const MyCart = () => {
                                     </div>
                                     <div>
                                         <h6 className='fw-bold'>Unit Price: {item.unit_price}</h6>
-                                        <h5 className='text-end'>Sub Total: <strong>{item.unit_price}</strong></h5>
+                                        <h5 className='text-end'>Sub Total: <strong>{item.unit_price * item.qty}</strong></h5>
                                     </div>
                                 </div>
                             </div>
